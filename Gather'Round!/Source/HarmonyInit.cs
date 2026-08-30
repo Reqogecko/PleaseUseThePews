a65a7b8ccdec9c267876ee83407847ea05fc2d86
Reqo <gekonida@gmail.com>
2026-08-30 10:37:26 +0200

    Delete Gather'Round! directory

--- Gather'Round!/Source/HarmonyInit.cs
+++
@@ -1,9 +0,0 @@
-using Verse;
-
-[StaticConstructorOnStartup]
-public class MinorChangesModStartup {
-    static MinorChangesModStartup() {
-        var harmony = new HarmonyLib.Harmony("net.reqo.RimWorld.MinorChanges");
-        harmony.PatchAll();
-    }
-}
